b80df40b04d4d55ac0d1222060219d6cc84d50a1
张伟 <zhang.wei5@jyall.com>
2017-05-04 11:25:21 +0800

    Initial commit

---
+++ Example/ZWBaseManager/ZWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZWAppDelegate.h
+//  ZWBaseManager
+//
+//  Created by weizhangCoder on 05/04/2017.
+//  Copyright (c) 2017 weizhangCoder. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/ZWBaseManager/ZWViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZWViewController.h
+//  ZWBaseManager
+//
+//  Created by weizhangCoder on 05/04/2017.
+//  Copyright (c) 2017 weizhangCoder. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZWViewController : UIViewController
+
+@end
